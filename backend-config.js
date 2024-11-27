76cc4fd853ff6a94157c17e1e5656410c0c286af
damqu44 <damian.charazka1@gmail.com>
2024-11-27 22:07:03 +0100

    update

--- backend-config.js
+++ backend-config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://www.store-backend-psi.vercel.app"
+export const BASE_URL = "https://store-backend-psi.vercel.app"
 // export const BASE_URL = "http://localhost:3000"
