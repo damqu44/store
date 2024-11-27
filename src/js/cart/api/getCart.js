ec4fcddb63fbb7197c83d743c7d6cc8a584d0dd1
damqu44 <damian.charazka1@gmail.com>
2024-11-27 22:22:51 +0100

    update

--- src/js/cart/api/getCart.js
+++ src/js/cart/api/getCart.js
@@ -1,23 +1,84 @@
-import { isAuthenticated } from "../../utils/auth"
-import { BASE_URL } from "../../../../backend-config"
+// import { isAuthenticated } from "../../utils/auth"
+// import { BASE_URL } from "../../../../backend-config"
+
+// export async function getCart() {
+//   let cartType = null
+
+//   if (isAuthenticated()) {
+//     cartType = ""
+//   } else {
+//     cartType = "cookies"
+//   }
+
+//   try {
+//     const response = await fetch(`${BASE_URL}/cart/${cartType}`, {
+//       method: "GET",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       credentials: "include",
+//     })
+
+//     if (!response.ok) {
+//       throw new Error("Failed to fetch cart data")
+//     }
+
+//     const data = await response.json()
+//     console.log(data)
+
+//     return data || []
+//   } catch (error) {
+//     console.error(error)
+//     return
+//   }
+// }
+// import { isAuthenticated } from "../../utils/auth"
+// import { BASE_URL } from "../../../../backend-config"
+
+// export async function getCart() {
+//   let cartType = null
+
+//   if (isAuthenticated()) {
+//     cartType = ""
+//   } else {
+//     cartType = "cookies"
+//   }
+
+//   try {
+//     const response = await fetch(`${BASE_URL}/cart/${cartType}`, {
+//       method: "GET",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       credentials: "include",
+//     })
+
+//     if (!response.ok) {
+//       throw new Error("Failed to fetch cart data")
+//     }
+
+//     const data = await response.json()
+//     console.log(data)
+
+//     return data || []
+//   } catch (error) {
+//     console.error(error)
+//     return
+//   }
+// }
 
 export async function getCart() {
-  let cartType = null
-
-  if (isAuthenticated()) {
-    cartType = ""
-  } else {
-    cartType = "cookies"
-  }
-
   try {
-    const response = await fetch(`${BASE_URL}/cart/${cartType}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      credentials: "include",
-    })
+    const response = await fetch(
+      `https://nodejs-serverless-function-express-eta-lac.vercel.app/api/hello`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      }
+    )
 
     if (!response.ok) {
       throw new Error("Failed to fetch cart data")
