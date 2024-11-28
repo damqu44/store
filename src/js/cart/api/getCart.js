6fd52b88e97493ceab4fa6c32e34494ee0dd6c80
damqu44 <damian.charazka1@gmail.com>
2024-11-28 21:44:55 +0100

    updatE

--- src/js/cart/api/getCart.js
+++ src/js/cart/api/getCart.js
@@ -32,37 +32,3 @@ export async function getCart() {
     return
   }
 }
-import { isAuthenticated } from "../../utils/auth"
-import { BASE_URL } from "../../../../backend-config"
-
-export async function getCart() {
-  let cartType = null
-
-  if (isAuthenticated()) {
-    cartType = ""
-  } else {
-    cartType = "cookies"
-  }
-
-  try {
-    const response = await fetch(`${BASE_URL}/cart/${cartType}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      credentials: "include",
-    })
-
-    if (!response.ok) {
-      throw new Error("Failed to fetch cart data")
-    }
-
-    const data = await response.json()
-    console.log(data)
-
-    return data || []
-  } catch (error) {
-    console.error(error)
-    return
-  }
-}
