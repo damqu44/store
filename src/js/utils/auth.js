40a967389d354ec17c19f03026ad0af038bd6c10
damqu44 <damian.charazka1@gmail.com>
2024-12-12 23:17:32 +0100

    update

--- src/js/utils/auth.js
+++ src/js/utils/auth.js
@@ -1,12 +1,15 @@
 export function isAuthenticated() {
   const cookies = document.cookie
     .split(";")
-    .map((cookie) => cookie.trim()) // Usuń białe znaki
+    .map((cookie) => cookie.trim())
     .reduce((acc, cookie) => {
       const [key, value] = cookie.split("=")
       acc[key] = value
       return acc
     }, {})
+
+  console.log("Ciasteczka:", cookies)
+  console.log("authToken:", cookies.authToken)
 
   return cookies.authToken !== undefined && cookies.authToken !== ""
 }
@@ -17,13 +20,12 @@ export function checkAuth() {
     if (currentUrl) {
       localStorage.setItem("redirectAfterLogin", currentUrl)
     }
-    // Przekierowanie do strony logowania
     // window.location.href = "/login.html";
   }
 }
 
 export function redirectAfterLogin() {
   const redirectUrl = localStorage.getItem("redirectAfterLogin") || "/"
-  localStorage.removeItem("redirectAfterLogin") // Usuń klucz po użyciu
+  localStorage.removeItem("redirectAfterLogin")
   window.location.href = redirectUrl
 }
