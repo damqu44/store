521aa98b5628c9fc99c885ed10262629bd4062b4
damqu44 <damian.charazka1@gmail.com>
2024-12-17 22:46:45 +0100

    update

--- src/js/pages/register/initRegister.js
+++ src/js/pages/register/initRegister.js
@@ -158,7 +158,7 @@ async function register({
   userLastName,
 }) {
   try {
-    const response = await fetch(`${BASE_URL}//auth/register`, {
+    const response = await fetch(`${BASE_URL}/auth/register`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
